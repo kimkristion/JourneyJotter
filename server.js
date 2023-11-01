76fe01bf93e71b30fdbb1ab491c248c1e0bdb038
Jack Lund <jacklund@Jacks-MacBook-Pro.local>
2023-10-31 20:53:31 -0700

    updated some server stuff and file structure as well as the homepage with handlebars

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
-app.use(express.static(path.join(__dirname, 'jack-home-page')));
+app.use(express.static(path.join(__dirname, './public')));
 app.use(require('./controllers/home-routes'));
 
 const db = mysql.createConnection(
