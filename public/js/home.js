219ffb12d613db59c2d4bf801d5a30f23c1be32a
Jack Lund <jacklund@Jacks-MacBook-Pro.local>
2023-11-02 21:36:39 -0700

    Made the home.hbs partially mobile friendly

--- public/js/home.js
+++ public/js/home.js
@@ -3,7 +3,11 @@ const inputBar = document.querySelector('#input-bar');
 const placeHolderText = [
     'Where did you go on your last trip?',
     'Where can you never go back to?',
-    'What is your favorite yearly trip?'
+    'What is your favorite yearly trip?',
+    "What's the most adventurous destination you've ever visited?",
+    'If you could teleport to any place in the world right now, where would it be?',
+    "What's your dream destination for a romantic getaway?",
+    "What's your go-to travel playlist or book for long journeys?", 
 ];
 
 let currentIndex = 0;
@@ -36,3 +40,85 @@ function eraseText() {
 document.addEventListener("DOMContentLoaded", (event) => {
     setTimeout(typeText, 0); // Start typing the first message immediately
 });
+
+/*const hamburger = document.querySelector('#hamburger');
+const navList = document.querySelector('#nav-list');
+function toggleNav() {
+    if (window.innerWidth <= 992) {
+        const navListStyle = window.getComputedStyle(navList);
+        if (navListStyle.display === 'none') {
+            navList.style.display = 'block';
+        } else if (navList.style.display == 'block'){
+            navList.style.display = 'none';
+        }
+    }
+}
+function reset(){
+    if (window.innerWidth > 992 && navList.style.display == 'none'){
+        navList.style.display = 'flex';
+    }
+}
+hamburger.addEventListener('click', toggleNav);
+window.addEventListener('resize', reset());*/
+/*const hamburger = document.querySelector('#hamburger');
+const navList = document.querySelector('#nav-list');
+let navListVisible = true; // Initially set to true for larger screens
+
+function toggleNav() {
+    if (window.innerWidth <= 992) {
+        if (navListVisible) {
+            navList.style.display = 'none';
+            navListVisible = false;
+        } else {
+            navList.style.display = 'block';
+            navListVisible = true;
+        }
+    }
+}
+
+function reset() {
+    if (window.innerWidth > 992) {
+        navList.style.display = 'flex';
+        navListVisible = true;
+    }
+}
+
+hamburger.addEventListener('click', toggleNav);
+window.addEventListener('resize', reset);
+
+// Initial check for screen size
+reset();
+
+*/
+const hamburger = document.querySelector('#hamburger');
+const navList = document.querySelector('#nav-list');
+let navListVisible = true; // Initially set to true for larger screens
+
+function toggleNav() {
+    if (window.innerWidth <= 992) {
+        if (!navListVisible) {
+            navList.style.display = 'flex';
+            navListVisible = true;
+        } else {
+            navList.style.display = 'none';
+            navListVisible = false;
+        }
+    }
+}
+
+function reset() {
+    if (window.innerWidth > 992) {
+        navList.style.display = 'flex';
+        navListVisible = true;
+    } else {
+        navList.style.display = 'none';
+        navListVisible = false;
+    }
+}
+
+
+hamburger.addEventListener('click', toggleNav);
+window.addEventListener('resize', reset);
+
+// Call reset function on page load to set the initial state
+reset();
--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const mysql = require('mysql2');
 require('dotenv').config();
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
@@ -14,17 +13,9 @@ app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 app.use(express.static(path.join(__dirname, './public')));
 app.use(require('./controllers/home-routes'));
+app.use(require('./controllers/login-routes'));
 
-const db = mysql.createConnection(
-    {
-
-        host: process.env.local_host,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME
-    },
-    console.log(`Connected to database`)
-);
+const db = require('./config/connection');
 
 app.use((req, res) => {
     res.status(404).end();
@@ -32,19 +23,6 @@ app.use((req, res) => {
 
 app.listen(PORT, () => {
     console.log(`Server running on: http://localhost:${PORT}`);
-});
-
-app.post('/api/users', async (req, res) => {
-    try {
-        const { username, email, password } = req.body;
-        const query = 'INSERT INTO users (username, email, password) VALUES (?, ?, ?)';
-        await db.query(query, [username, email, password]);
-
-        res.status(200).json({ message: 'User registered successfully' });
-    } catch (error) {
-        console.error('Error registering user:', error);
-        res.status(500).json({ error: 'Internal server error' });
-    }
 });
 
 db.connect((err) => {
