0d531f33b21767286d16e2618ad564d07642b623
birdslisten <justin.schultz37@gmail.com>
2023-11-02 20:32:52 -0700

    modularized connection and made successful POST call to SQL db from login-routes

--- controllers/login-routes.js
+++ controllers/login-routes.js
@@ -1,7 +1,32 @@
 const router = require('express').Router();
+const db = require('../config/connection'); // Adjust the import path as needed
 
+// GET route to render the login form
 router.get('/login', async (req, res) => {
     res.render('login');
+});
+
+// POST route to handle form submissions
+router.post('/login', (req, res) => {
+    const { username, email, password } = req.body;
+    const query = 'INSERT INTO users (username, email, password) VALUES (?, ?, ?)';
+
+    new Promise((resolve, reject) => {
+        db.execute(query, [username, email, password], (err, results) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(results);
+            }
+        });
+    })
+        .then(() => {
+            res.status(200).json({ message: 'User registered successfully' });
+        })
+        .catch((error) => {
+            console.error('Error registering user:', error);
+            res.status(500).json({ error: 'Internal server error' });
+        });
 });
 
 module.exports = router;
