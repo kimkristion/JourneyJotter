5295af545fb3f3314aa5f00067aef549f7d1530a
birdslisten <justin.schultz37@gmail.com>
2023-11-06 21:19:21 -0800

    added packages for sessions and session storage, added index.js and updated logs model

--- db/schema.sql
+++ db/schema.sql
@@ -1,22 +1,3 @@
-DROP DATABASE IF EXISTS test_db;
-
-CREATE DATABASE test_db;
-USE test_db;
-
-CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL
-);
-
-CREATE TABLE users_input (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT NOT NULL, 
-    feeling VARCHAR(50) NOT NULL,
-    place_description TEXT NOT NULL,
-    rating INT NOT NULL,
-    FOREIGN KEY (user_id) REFERNCES users(id),
-    CHECK (rating >= 1 AND rating <= 5)
-);
+DROP DATABASE IF EXISTS jotter_db;
+CREATE DATABASE jotter_db;
 
--- server.js
+++ server.js
@@ -6,6 +6,21 @@ const path = require('path');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+const sequelize = require('./config/connection');
+const session = require('express-session'); // Added session import
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(session(sess));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
@@ -14,9 +29,6 @@ app.set('view engine', 'handlebars');
 app.use(express.static(path.join(__dirname, './public')));
 app.use(require('./controllers/home-routes'));
 app.use(require('./controllers/login-routes'));
-app.use(require('./controllers/logpage-routes'));
-
-const sequelize = require('./config/connection');
 
 app.use((req, res) => {
     res.status(404).end();
